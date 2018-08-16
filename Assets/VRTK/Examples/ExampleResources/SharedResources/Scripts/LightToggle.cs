c4d529f102490a1feef9ef57eabc1c9ceb8f5b84
twosheep0603 <mike830603@gmail.com>
2018-08-16 17:30:20 +0800

    20180816進度

---
+++ Assets/VRTK/Examples/ExampleResources/SharedResources/Scripts/LightToggle.cs
@@ -0,0 +1,17 @@
+﻿namespace VRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    public class LightToggle : MonoBehaviour
+    {
+        public Light mylight;
+        public VRTK_ControllerEvents controllerEvents;
+        public VRTK_ControllerEvents.ButtonAlias toggleButton = VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress;
+
+    
+        void Update() {
+        Debug.Log("checked"+toggleButton);
+        }
+    }
+}
---
+++ Assets/toggle.cs
@@ -0,0 +1,31 @@
+﻿namespace VRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    public class toggle : MonoBehaviour {
+
+        public Light myLight;
+        public VRTK_ControllerEvents controllerEvents;
+        public VRTK_ControllerEvents.ButtonAlias toggleButton = VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress;
+        bool enable = true;
+        /*
+        void Start()
+        {
+            Debug.Log("Start");
+        }*/ 
+        
+                // Update is called once per frame
+        void Update () {
+            
+            if(controllerEvents.IsButtonPressed(toggleButton)) {
+                Debug.Log("pressed.");
+                if (enable){
+                    myLight.enabled = !myLight.enabled;
+                }
+                enable = false;
+            }
+            else enable = true;
+        }
+    }
+}
